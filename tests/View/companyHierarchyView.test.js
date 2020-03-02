da147b065360bfc6798a9cc85ac5678f56ca3288
Viraj Dayarathne <viraj_dayarathne@yahoo.com>
2020-03-03 06:40:03 +1100

    Update more tests

--- tests/View/companyHierarchyView.test.js
+++ tests/View/companyHierarchyView.test.js
@@ -1,6 +1,50 @@
 import CompanyHierarchyView from '../../src/View/companyHierarchyView';
+import Employee from '../../src/Model/employee';
 
-it('Company Controller should initialize', () => {
-  const companyHierarchyView = new CompanyHierarchyView();
-  expect(companyHierarchyView).toBeTruthy();
+describe('Company Hierarchy View', () => {
+  it('should initialize', () => {
+    const companyHierarchyView = new CompanyHierarchyView();
+    expect(companyHierarchyView).toBeTruthy();
+  });
+
+  it('displayCompanyHierarchy should show hierarchy', () => {
+
+    const top = new Employee({
+      "id": 1,
+      "name": "Top",
+      "managerId": null
+    });
+
+    const middle = new Employee({
+      "id": 2,
+      "name": "Middle",
+      "managerId": 1
+    });
+
+    const bottom1 = new Employee({
+      "id": 3,
+      "name": "Bottom 1",
+      "managerId": 2
+    });
+
+    const bottom2 = new Employee({
+      "id": 4,
+      "name": "Bottom 2",
+      "managerId": 2
+    });
+
+    middle.addSubordinate(bottom1);
+    middle.addSubordinate(bottom2);
+    top.addSubordinate(middle);
+
+    const hierarchy = {
+      1: top
+    };
+
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    CompanyHierarchyView.displayCompanyHierarchy(hierarchy);
+
+    expect(consoleSpy).toHaveBeenCalledTimes(4);
+  });
 });
