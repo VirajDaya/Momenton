8baedc4ef90bf1702584f91b3e9f1fab0a4f0df9
Viraj Dayarathne <viraj_dayarathne@yahoo.com>
2020-03-03 06:41:57 +1100

    Update more more tests

--- tests/Controller/companyController.test.js
+++ tests/Controller/companyController.test.js
@@ -1,6 +1,8 @@
 import CompanyController from '../../src/Controller/companyController';
 
-it('Company Controller should initialize', () => {
-  const companyController = new CompanyController();
-  expect(companyController).toBeTruthy();
+describe('Company Controller', () => {
+  it('should initialize', () => {
+    const companyController = new CompanyController();
+    expect(companyController).toBeTruthy();
+  });
 });
